e630bc28e73b624c47abfb14f6336a4f6fce9f80
rizkiraden <rizkiraden616@gmail.com>
2025-06-25 11:58:58 +0700

    Menambahkan Router Vue App

---
+++ vue-app/src/views/HomeIndex.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="container mt-custom">
+      <div class="bg-light p-5 rounded">
+        <h1>Teva</h1>
+        <p class="lead">Belajar FullStack Express.js dan Vue.js oleh <strong>Raden Rizki</strong></p>
+        <a class="btn btn-lg btn-primary" href="https://github.com/raden616Rizki" target="_blank" role="button">SELENGKAPNYA</a>
+      </div>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
